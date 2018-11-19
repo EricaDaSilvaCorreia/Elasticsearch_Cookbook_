702e88f5861f8dfd64b85ebd4c860a0224e00ce6
Tech-A60 <tech-a60@Tech-A60s-MacBook-Air.local>
2018-11-19 10:57:52 +0000

    MAAK 1.0: Unit Tests

--- elasticsearch/spec/unit/recipes/default_spec.rb
+++ elasticsearch/spec/unit/recipes/default_spec.rb
@@ -11,25 +11,49 @@ describe 'elasticsearch::default' do
     let(:chef_run) do
       # for a complete list of available platforms and versions see:
       # https://github.com/customink/fauxhai/blob/master/PLATFORMS.md
-      runner = ChefSpec::ServerRunner.new(platform: 'ubuntu', version: '16.04')
+      runner = ChefSpec::Solo.new(platform: 'ubuntu', version: '16.04')
       runner.converge(described_recipe)
     end
 
     it 'converges successfully' do
       expect { chef_run }.to_not raise_error
     end
-  end
 
-  context 'When all attributes are default, on CentOS 7.4.1708' do
-    let(:chef_run) do
-      # for a complete list of available platforms and versions see:
-      # https://github.com/customink/fauxhai/blob/master/PLATFORMS.md
-      runner = ChefSpec::ServerRunner.new(platform: 'centos', version: '7.4.1708')
-      runner.converge(described_recipe)
+    it "runs apt get update" do
+      expect(chef_run).to update_apt_update 'update_sources'
     end
 
-    it 'converges successfully' do
-      expect { chef_run }.to_not raise_error
+    it 'should install java' do
+      expect(chef_run).to upgrade_package "default-jre"
     end
+
+    it 'should install elasticsearch' do
+      expect(chef_run).to upgrade_package "elasticsearch"
+    end
+
+    it "should delete /etc/elasticsearch/elasticsearch.yml" do
+     expect(chef_run).to delete_link('/etc/elasticsearch/elasticsearch.yml')
+   end
+
+    it "should delete /etc/elasticsearch/jvm.options" do
+     expect(chef_run).to delete_link('/etc/elasticsearch/jvm.options')
+   end
+
+   it "should create file elasticsearch.yml in /etc/elasticsearch/" do
+      expect(chef_run).to create_template('/etc/elasticsearch/elasticsearch.yml')
+    end
+
+   it "should create file jvm.options in /etc/elasticsearch/" do
+      expect(chef_run).to create_template('/etc/elasticsearch/jvm.options')
+    end
+
+    it 'should enable the elasticsearch service' do
+      expect(chef_run).to enable_service "elasticsearch"
+    end
+
+    it 'should start the elasticsearch service' do
+      expect(chef_run).to start_service "elasticsearch"
+    end
+    
   end
 end
