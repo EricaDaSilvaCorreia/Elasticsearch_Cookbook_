4133b7e560d0d220601ab5cf47d63719d72a2a79
Tech-A60 <tech-a60@Tech-A60s-MacBook-Air.local>
2018-11-19 11:27:55 +0000

    MAAK 2.0 : Recipes to be checked

--- elasticsearch/recipes/default.rb
+++ elasticsearch/recipes/default.rb
@@ -3,3 +3,45 @@
 # Recipe:: default
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
+apt_update 'update_sources' do
+  action :update
+end
+
+package 'default-jre'
+package 'elasticsearch'
+package 'curl'
+
+# apt_repository 'mongodb-org' do
+#   uri "http://repo.mongodb.org/apt/ubuntu"
+#   distribution "xenial/mongodb-org/3.2"
+#   components ['multiverse']
+#   keyserver "hkp://keyserver.ubuntu.com:80"
+#   key "EA312927"
+# end
+
+package 'elasticsearch' do
+  action :upgrade
+end
+
+service 'elasticsearch' do
+  supports status: true, restart: true, reload: true
+  action [:enable, :start]
+end
+
+link '/etc/elasticsearch/elasticsearch.yml' do
+  action :delete
+end
+
+link '/etc/elasticsearch/jvm.options' do
+  action :delete
+end
+
+template '/etc/elasticsearch/elasticsearch.yml' do
+  source 'elasticsearch.yml.erb'
+  notifies :restart, 'service[elasticsearch]'
+end
+
+template '/etc/elasticsearch/jvm.options' do
+  source 'jvm.options.erb'
+  notifies :restart, 'service[elasticsearch]'
+end
